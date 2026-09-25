99261846ef4003059300798a2ea88e6ad9b4be2d
Madhav-24 <madhav.sudalaimani@gmail.com>
2026-09-25 10:48:19 +0530

    vercel_deploynment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 // React's Vite plugin supplies JSX transformation and development tooling.
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/demo_webpage',
+  base: process.env.VITE_BASE_PATH || "/demo_webpage",
 })
